721062a0d34e8c9c0397e47b7ba0dac5be84c1b5
laivwxyz <delacruzlailanie2004@gmail.com>
2023-07-24 16:34:58 +0800

    Creating the application, title, size of the main window

--- add_entry.py
+++ add_entry.py
@@ -7,3 +7,9 @@ from tkinter import Label, Entry, Text, Button, StringVar
 
 #Creating a class named AddEntryWindow
 class AddEntryWindow:
+    def __init__(self):
+        self.window = Toplevel()
+        self.window.title("Add Entry")
+        self.window.geometry("925x780+300+200")
+        self.window.configure(bg='#326273')
+        self.window.resizable(False,False)
