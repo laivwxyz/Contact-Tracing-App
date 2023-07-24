7ade8bc4ac0fa5bb08c63b3cd643eb2c346c2db9
laivwxyz <delacruzlailanie2004@gmail.com>
2023-07-24 16:37:52 +0800

    Create background image for add entry

--- add_entry.py
+++ add_entry.py
@@ -13,3 +13,10 @@ class AddEntryWindow:
         self.window.geometry("925x780+300+200")
         self.window.configure(bg='#326273')
         self.window.resizable(False,False)
+
+        # Create background image for add entry 
+        self.image = Image.open("add_entry_background.png")  
+        self.bg_image = ImageTk.PhotoImage(self.image)
+
+        self.bg_label = Label(self.window, image=self.bg_image)  
+        self.bg_label.place(x=0, y=0, relwidth=1, relheight=1)
