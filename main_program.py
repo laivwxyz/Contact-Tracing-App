06e7da676f7a8d588f2b2cb51b9b441f9fe6b448
laivwxyz <delacruzlailanie2004@gmail.com>
2023-07-24 19:39:44 +0800

    Run the program

--- main_program.py
+++ main_program.py
@@ -1,3 +1,9 @@
 # Importing important details
 from tkinter import *
 from contact_tracing_app import ContactTracingApp
+
+# Run the program
+if __name__ == "__main__":
+    window = Tk()
+    main = ContactTracingApp(window)
+    window.mainloop()
