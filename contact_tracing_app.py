68280285b56a98116ddafbe815a8c7a508efbe03
laivwxyz <delacruzlailanie2004@gmail.com>
2023-07-24 19:28:07 +0800

    Creating a class named ContactTracingApp

--- contact_tracing_app.py
+++ contact_tracing_app.py
@@ -3,3 +3,7 @@ from tkinter import *
 from add_entry import AddEntryWindow
 from search_entry import SearchEntryWindow
 from PIL import Image, ImageTk
+
+# Creating a class named ContactTracingApp
+class ContactTracingApp:
+    def __init__(self, window):
