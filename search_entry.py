893899d2421e2110666dcf85ab59ef6341438e5f
laivwxyz <delacruzlailanie2004@gmail.com>
2023-07-24 18:37:53 +0800

    Creating the application, title, size of the main window

--- search_entry.py
+++ search_entry.py
@@ -7,3 +7,8 @@ from tkinter import Label, Entry, Text, Button
 # Creating a class named SearchEntryWindow
 class SearchEntryWindow:
     def __init__(self):
+        self.window = Toplevel()
+        self.window.title("Search Entry")
+        self.window.geometry("925x780")
+        self.window.configure(bg='#326273')
+        self.window.resizable(False, False)
