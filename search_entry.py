5f8322daefb49d01ab60e005df16c5fab567e4bb
laivwxyz <delacruzlailanie2004@gmail.com>
2023-07-24 19:20:46 +0800

    Other code need in search entry window code

--- search_entry.py
+++ search_entry.py
@@ -35,3 +35,53 @@ class SearchEntryWindow:
     
         # Exit Button
         Button(self.window, text="Exit", bg="cyan", fg="black", width=15, height=2, command=self.exit).place(x=465, y=670)
+
+    def search_entries(self):
+        query = self.search_entry.get().strip() 
+        if not query:
+            # Show a pop-up warning message if the search query is empty
+            messagebox.showwarning("No Search Query", "Please enter a search query.")
+            return
+
+        search_results = self.search_in_file(query)
+
+        self.result_text.delete(1.0, END)
+        if search_results:
+            for result in search_results:
+                self.result_text.insert(END, result)
+                self.result_text.insert(END, "\n\n")
+
+            # Show a pop-up message with the number of matching entries found
+            messagebox.showinfo("Search Results", f"{len(search_results)} matching entries found.")
+        else:
+            self.result_text.insert(END, "No matching entries found.")
+            # Show a pop-up warning message if no matching entries are found
+            messagebox.showwarning("No Match", "No matching entries found for the search query.")
+
+    def search_in_file(self, query):
+        matching_entries = []
+
+        try:
+            with open("covid_contact_tracing.txt", "r") as file:
+                content = file.read()
+                entries = content.split("-----------------------------\n")
+                for entry in entries:
+                    if query.lower() in entry.lower():
+                        matching_entries.append(entry)
+        except FileNotFoundError:
+            # Show a pop-up error message if the file is not found
+            messagebox.showerror("Error", "File not found. No entries added yet.")
+        except Exception as e:
+            # Show a pop-up error message for any other unexpected errors
+            messagebox.showerror("Error", f"An error occurred: {str(e)}")
+
+        return matching_entries
+    
+    def clear(self):
+        # Clear the search entry and result text widgets
+        self.search_entry.delete(0, END)
+        self.result_text.delete(1.0, END)
+
+    def exit(self):
+        # Close the application
+        self.window.destroy()
