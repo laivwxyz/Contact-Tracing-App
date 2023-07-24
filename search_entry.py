11dcc799c622e73759116c6bb2e140139f29434b
laivwxyz <delacruzlailanie2004@gmail.com>
2023-07-24 19:14:55 +0800

    Create background image for search entry

--- search_entry.py
+++ search_entry.py
@@ -12,3 +12,10 @@ class SearchEntryWindow:
         self.window.geometry("925x780")
         self.window.configure(bg='#326273')
         self.window.resizable(False, False)
+
+        # Create background image for search entry
+        self.image = Image.open("search_entry_background.png")  
+        self.bg_image = ImageTk.PhotoImage(self.image)
+
+        self.bg_label = Label(self.window, image=self.bg_image)  
+        self.bg_label.place(x=0, y=0, relwidth=1, relheight=1)
