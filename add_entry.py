34d0739686851470144ce6d1f46f1b4c00316ab5
laivwxyz <delacruzlailanie2004@gmail.com>
2023-07-24 16:31:53 +0800

    Importing important details

--- add_entry.py
+++ add_entry.py
@@ -1,2 +1,9 @@
+# Importing important details
+from tkinter import *
+from PIL import Image, ImageTk
+from tkinter.ttk import Combobox
+from tkinter import messagebox
+from tkinter import Label, Entry, Text, Button, StringVar
+
 #Creating a class named AddEntryWindow
 class AddEntryWindow:
