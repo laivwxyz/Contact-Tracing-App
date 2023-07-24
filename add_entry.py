d4c152d076ee6e04159a356ad200f509a3ec1509
laivwxyz <delacruzlailanie2004@gmail.com>
2023-07-24 17:45:51 +0800

    Date of birth Label

--- add_entry.py
+++ add_entry.py
@@ -33,3 +33,6 @@ class AddEntryWindow:
 
         # Gender Label
         Label (self.window, text="GENDER", font = ('Perpetua', 18, "bold"), bg= "cyan").place(x=485, y=220)
+
+        # Date of birth Label
+        Label (self.window, text="DATE OF BIRTH", font = ('Perpetua', 18, "bold"), bg= "cyan").place(x=90, y=270)
