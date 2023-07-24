54170265678754bb7c82852711bc07e75b8a9821
laivwxyz <delacruzlailanie2004@gmail.com>
2023-07-24 18:36:54 +0800

    Creating a class named SearchEntryWindow

--- search_entry.py
+++ search_entry.py
@@ -3,3 +3,7 @@ from tkinter import *
 from tkinter import messagebox
 from PIL import Image, ImageTk
 from tkinter import Label, Entry, Text, Button
+
+# Creating a class named SearchEntryWindow
+class SearchEntryWindow:
+    def __init__(self):
