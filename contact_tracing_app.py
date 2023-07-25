bf7ffa8ab12b89b8519e44090d026b93f185a82f
laivwxyz <delacruzlailanie2004@gmail.com>
2023-07-25 22:10:34 +0800

    It's now done, you can check it out

--- contact_tracing_app.py
+++ contact_tracing_app.py
@@ -14,18 +14,18 @@ class ContactTracingApp:
         self.window.resizable(False, False)
     
         # Create background image for front page
-        self.image = Image.open("contact_tracing_app_background.png")  
+        self.image = Image.open('contact_tracing_app_background.png')  
         self.bg_image = ImageTk.PhotoImage(self.image)
 
         self.bg_label = Label(self.window, image=self.bg_image) 
         self.bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 
         # Add entry button
-        self.add_button = Button(self.window, text="Add Information",bg='cyan',command=self.open_add_entry_window, width=24, height=1, font=("Times New Roman", 20))
+        self.add_button = Button(self.window, text='Add Information',bg='cyan',command=self.open_add_entry_window, width=24, height=1, font=('Times New Roman', 20))
         self.add_button.place(x=91, y=269)
 
         # Search entry button
-        self.search_button = Button(self.window, text="Search Existing Information", bg = 'cyan', command=self.open_search_entry_window, width=24, height=1, font=("Times New Roman", 20))
+        self.search_button = Button(self.window, text='Search Existing Information', bg = 'cyan', command=self.open_search_entry_window, width=24, height=1, font=('Times New Roman', 20))
         self.search_button.place(x=477, y=269)
 
     # Open add entry window
