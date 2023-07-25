5b0ab968d50ea3a1f49b11572b638ea5f1bccfb0
laivwxyz <delacruzlailanie2004@gmail.com>
2023-07-25 22:09:29 +0800

    It's now done, you can check it out

--- search_entry.py
+++ search_entry.py
@@ -8,39 +8,39 @@ from tkinter import Label, Entry, Text, Button
 class SearchEntryWindow:
     def __init__(self):
         self.window = Toplevel()
-        self.window.title("Search Entry")
-        self.window.geometry("925x780")
+        self.window.title('Search Entry')
+        self.window.geometry('925x780')
         self.window.configure(bg='#326273')
         self.window.resizable(False, False)
 
         # Create background image for search entry
-        self.image = Image.open("search_entry_background.png")  
+        self.image = Image.open('search_entry_background.png')  
         self.bg_image = ImageTk.PhotoImage(self.image)
 
         self.bg_label = Label(self.window, image=self.bg_image)  
         self.bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 
         # Entry for search
-        self.search_entry = Entry(self.window, font=("Perpetua", 15), bd=2, width=40)
+        self.search_entry = Entry(self.window, font=('Perpetua', 15), bd=2, width=40)
         self.search_entry.place(x=260, y=180)
 
-        self.search_button = Button(self.window, text="Search", bg="cyan", fg="black", width=15, height=2, command=self.search_entries)
+        self.search_button = Button(self.window, text='Search', bg='cyan', fg='black', width=15, height=2, command=self.search_entries)
         self.search_button.place(x=400, y=230)
 
-        self.result_text = Text(self.window, width=50, height=15, wrap=WORD, font=("Perpetua", 15), bd=2)
+        self.result_text = Text(self.window, width=50, height=15, wrap=WORD, font=('Perpetua', 15), bd=2)
         self.result_text.place(x=210, y=300)
 
         # Clear Button
-        Button(self.window, text="Clear", bg="cyan", fg="black", width=15, height=2, command=self.clear).place(x=300, y=670)
+        Button(self.window, text='Clear', bg='cyan', fg='black', width=15, height=2, command=self.clear).place(x=300, y=670)
     
         # Exit Button
-        Button(self.window, text="Exit", bg="cyan", fg="black", width=15, height=2, command=self.exit).place(x=465, y=670)
+        Button(self.window, text='Exit', bg='cyan', fg='black', width=15, height=2, command=self.exit).place(x=465, y=670)
 
     def search_entries(self):
         query = self.search_entry.get().strip() 
         if not query:
             # Show a pop-up warning message if the search query is empty
-            messagebox.showwarning("No Search Query", "Please enter a search query.")
+            messagebox.showwarning('No Search Query', 'Please enter a search query.')
             return
 
         search_results = self.search_in_file(query)
@@ -52,17 +52,17 @@ class SearchEntryWindow:
                 self.result_text.insert(END, "\n\n")
 
             # Show a pop-up message with the number of matching entries found
-            messagebox.showinfo("Search Results", f"{len(search_results)} matching entries found.")
+            messagebox.showinfo('Search Results', f'{len(search_results)} matching entries found.')
         else:
-            self.result_text.insert(END, "No matching entries found.")
+            self.result_text.insert(END, 'No matching entries found.')
             # Show a pop-up warning message if no matching entries are found
-            messagebox.showwarning("No Match", "No matching entries found for the search query.")
+            messagebox.showwarning('No Match', 'No matching entries found for the search query.')
 
     def search_in_file(self, query):
         matching_entries = []
 
         try:
-            with open("covid_contact_tracing.txt", "r") as file:
+            with open('covid_contact_tracing.txt', 'r') as file:
                 content = file.read()
                 entries = content.split("-----------------------------\n")
                 for entry in entries:
@@ -70,10 +70,10 @@ class SearchEntryWindow:
                         matching_entries.append(entry)
         except FileNotFoundError:
             # Show a pop-up error message if the file is not found
-            messagebox.showerror("Error", "File not found. No entries added yet.")
+            messagebox.showerror('Error', 'File not found. No entries added yet.')
         except Exception as e:
             # Show a pop-up error message for any other unexpected errors
-            messagebox.showerror("Error", f"An error occurred: {str(e)}")
+            messagebox.showerror('Error', f'An error occurred: {str(e)}')
 
         return matching_entries
     
